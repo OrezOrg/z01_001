2be648e73827afdb009842c7039d1c3d03b3daf1
Dark Zero <OrezOrg@users.noreply.github.com>
2017-02-13 13:06:05 -0600

    02132017 - Proyecto inicial cargado
    Proyecto inicial

---
+++ Calculadora/Forms/zMain.vb
@@ -0,0 +1,16 @@
+﻿Public Class zMain
+    Public zTitle As String = Me.Text
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Eres un rebelde, me agradas...")
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("A mi ni me mires, no dije que hicieras nada...")
+    End Sub
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            Me.Text = zTitle & " Wolas"
+        Else
+            Me.Text = zTitle
+        End If
+    End Sub
+End Class
